a6b6f1c13491e8a8ced3284de0e3d1f602be2a32
威纳信息-曾志强 <qiangzi@via.cool>
2021-04-02 15:40:24 +0800

    提交

---
+++ component/p-home/default/default.js
@@ -0,0 +1,21 @@
+// component/p-home/default/default.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		jumpIndex() {
+			this.triggerEvent("jumpIndex", {}, {});
+		},
+	},
+});
---
+++ pages/home/index.js
@@ -0,0 +1,9 @@
+Page({
+	onLoad() {},
+	jumpIndex() {
+		// 跳转到其他页面
+		wx.navigateTo({
+			url: "../index/index",
+		});
+	},
+});
